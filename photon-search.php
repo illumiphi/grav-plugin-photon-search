17c563685784492e4e53faa2271f2c7a97df18e1
i-am-phi <phi@i-am-phi.net>
2020-08-17 07:35:13 -0700

    cleanup search tempalte

--- photon-search.php
+++ photon-search.php
@@ -85,12 +85,18 @@ class PhotonSearchPlugin extends Plugin
 
         // styles
         if ($this->config->get('plugins.photon-search.built_in_css')) {
+          $css = 'plugin://tntsearch/assets/tntsearch.css';
+          // $assets->addCss($css);
+
           $css = 'plugin://photon-search/assets/search.css';
           $assets->addCss($css, 100, false, 'photon-plugins' );
         }
 
         // scripts
         if ($this->config->get('plugins.photon-search.built_in_js')) {
+          $js = 'plugin://tntsearch/assets/tntsearch.js';
+          // $assets->addJs($js);
+          
           $js = 'plugin://photon-search/assets/search.js';
           $assets->addJs($js, 100, false, 'defer', 'photon-plugins' );
         }
